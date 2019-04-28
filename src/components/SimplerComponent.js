8ddd9eb3fd436c294bcb403c51bfd3244e2ff469
github username <github email address>
2019-04-28 13:10:21 +0100

    Done.

--- src/components/SimplerComponent.js
+++ src/components/SimplerComponent.js
@@ -1,1 +1,10 @@
-// Code SimplerComponent Here
+import React from "react";
+
+
+
+const happyEl = props => (
+  <div onClick={props.handleClick}>
+    <p>I am just happy</p>
+  </div>
+);
+export default happyEl
